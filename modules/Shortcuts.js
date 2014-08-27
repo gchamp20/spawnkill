1ac83e90d809788055d9f2ccea8bef902338e1c8
Thiht <thiht44@gmail.com>
2014-08-27 23:36:35 +0200

    ; manquant

--- modules/Shortcuts.js
+++ modules/Shortcuts.js
@@ -85,7 +85,7 @@ SK.moduleConstructors.Shortcuts.prototype.init = function() {
 			// Si l'on se trouve dans un champ de texte, on désactive les raccourcis (voir https://github.com/dorian-marchal/spawnkill/issues/30)
 			var target = event.target || event.srcElement;
 			if (target.tagName === "TEXTAREA" || (target.tagName === "INPUT" && target.type === "text")) {
-				return
+				return;
 			}
 
 			//Ctrl + fleche gauche -> page précedente
