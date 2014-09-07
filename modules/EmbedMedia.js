f339840014c6e9b2e1e07e06e2a35c62994817d1
dorian-marchal <marchal.dorian@gmail.com>
2014-09-07 15:41:56 +0200

    Corrige l'intégration des images avec des querystrings ou des majuscules
    Regex de test : http://regex101.com/r/fD7bC3/1

--- modules/EmbedMedia.js
+++ modules/EmbedMedia.js
@@ -75,8 +75,9 @@ SK.moduleConstructors.EmbedMedia.prototype.initMediaTypes = function() {
          * ne match pas :
          * https://www.dropbox.com/s/lfta3yd9tryotxp/screen2.png
          * http://www.hostingpics.net/viewer.php?id=785683.jpg
+         * Pour les tests : http://regex101.com/r/fD7bC3/1
          */
-        regex: /^https?:\/\/(?!www\.dropbox\.com|www\.hostingpics)(?:(?:(www\.hapshack\.com\/\?v=)|(www\.noelshack\.com\/([\d]{4})-([\d]{2})-))?(.*.(jpe?g|png|gif)))$/,
+        regex: /^https?:\/\/(?!www\.dropbox\.com|www\.hostingpics)(?:(?:(www\.hapshack\.com\/\?v=)|(www\.noelshack\.com\/([\d]{4})-([\d]{2})-))?(.*.(jpe?g|png|gif|JPE?G|PNG|GIF)))(?:\?.*)?$/,
 
         addHideButton: true,
         showButtonText: "Afficher les images",
