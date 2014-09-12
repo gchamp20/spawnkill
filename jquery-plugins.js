580c56b890e3f7d176b000c1acb8ce2375815ee6
dorian-marchal <marchal.dorian@gmail.com>
2014-09-12 16:33:30 +0200

    Ajoute la fonction $.fn.isOversized

--- jquery-plugins.js
+++ jquery-plugins.js
@@ -30,22 +30,40 @@ $.fn.scrollThere = function(delay, callback) {
 };
 
 /**
- * Retourne vrai si l'élément est visible à l'écran.
- * @param {boolean} partial Quand vrai, on retourne vrai si l'élément est partiellement visible
- * @return {boolean} Vrai si l'élément est visible
+ * Retourne true si l'élément est visible à l'écran.
+ * @param {boolean} partial Quand true, on retourne true si l'élément est partiellement visible
+ * @return {boolean} true si l'élément est visible
  */
 $.fn.isVisibleOnScreen = function(partial) {
 
-    var $this = $(this),
-        $window = $(window),
-        windowHeight = $window.height(),
-        viewTop = $window.scrollTop(),
-        viewBottom = viewTop + windowHeight,
-        _top = $this.offset().top,
-        _bottom = _top + $this.height(),
-        compareTop = partial ? _bottom : _top,
-        compareBottom = partial ? _top : _bottom;
+    var $this = $(this);
+    var $window = $(window);
+    var windowHeight = $window.height();
+    var viewTop = $window.scrollTop();
+    var viewBottom = viewTop + windowHeight;
+    var _top = $this.offset().top;
+    var _bottom = _top + $this.height();
+    var compareTop = partial ? _bottom : _top;
+    var compareBottom = partial ? _top : _bottom;
 
     return (compareBottom <= viewBottom) && (compareTop >= viewTop);
 
+};
+
+
+/**
+ * @param {int} coefficient Si renseigné, la fonction retourne true quand
+ *   la hauteur de l'élément dépasse <coefficient>% de la hauteur de l'écran
+ * @return {boolean} true si l'élément est plus haut que l'écran
+ */
+$.fn.isOversized = function(coefficient) {
+
+    coefficient = coefficient || 100;
+    coefficient = coefficient / 100;
+
+    var elementHeight = $(this).outerHeight();
+    var windowHeight = $(window).height();
+    var maxHeight = windowHeight * coefficient;
+
+    return elementHeight > maxHeight;
 };
