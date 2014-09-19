53a41ef564a498971b45f8b95c7cf66e9000fc6b
dorian-marchal <marchal.dorian@gmail.com>
2014-09-20 01:34:22 +0200

    Ajoute le client de mise à jour aux scripts de démarrage/stoppage

--- server/socket/bin/start-server.sh
+++ server/socket/bin/start-server.sh
@@ -2,4 +2,5 @@
 php spawnkill-server.php &
 php update-topics-server.php &
 node update-to-server-link.js &
+node update-topics-client.js &
 echo "Serveur SpawnKill démarré."
--- server/socket/bin/stop-server.sh
+++ server/socket/bin/stop-server.sh
@@ -2,4 +2,5 @@
 ps aux | grep "spawnkill-server.php" | awk '{print $2}' | xargs kill -9 &>/dev/null
 ps aux | grep "update-topics-server.php" | awk '{print $2}' | xargs kill -9 &>/dev/null
 ps aux | grep "update-to-server-link.js" | awk '{print $2}' | xargs kill -9 &>/dev/null
+ps aux | grep "update-topics-client.js" | awk '{print $2}' | xargs kill -9 &>/dev/null
 echo "Serveur SpawnKill stoppé."
