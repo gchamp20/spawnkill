52bb459623f01bcb139256e71235965bb383ce09
dorian-marchal <marchal.dorian@gmail.com>
2014-09-21 10:05:41 +0200

    Ajoute un délai au script de démarrage du serveur
    Evite les cas où le lien est lancé avant que les deux serveurs Main et
    Update soient prêts.

--- server/socket/bin/start-server.sh
+++ server/socket/bin/start-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 php spawnkill-server.php &
 php update-topics-server.php &
+sleep 0.5
 node update-to-server-link.js &
+sleep 1
 node update-topics-client.js &
 echo "Serveur SpawnKill démarré."
