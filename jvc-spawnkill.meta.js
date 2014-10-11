590f933516db9fb365659a4c7ef1a1224c499655
dorian-marchal <marchal.dorian@gmail.com>
2014-10-11 17:46:49 +0200

    Passe le namespace de www.spixel.fr à www.spawnkill.fr

--- jvc-spawnkill.meta.js
+++ jvc-spawnkill.meta.js
@@ -2,7 +2,7 @@
 // @name        JVC SpawnKill
 // @description JVC SpawnKill est un plugin pour jeuxvideo.com ajoutant des fonctionnalités comme les avatars, les citations ou les signatures.
 // @author      Spixel_
-// @namespace   http://www.spixel.fr
+// @namespace   http://www.spawnkill.fr
 // @include     http://*.jeuxvideo.com*
-// @version     1.15
+// @version     1.15.0.1
 // ==/UserScript==
