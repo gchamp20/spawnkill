be8915b96b3fd78499964bc1f7d5d31281d97487
dorian-marchal <marchal.dorian@gmail.com>
2014-10-24 22:12:55 +0200

    Supprime le fichier de configuration du dev local du dépôt

--- other/local-dev/local-dev-config.sh
+++ other/local-dev/local-dev-config.sh
@@ -1,3 +1,10 @@
+# répertoire greasemonkey du script
+# /!\ Tout ce qui se trouve dans ce répertoire sera supprimé !
+# Veillez à ne pas vous tromper en saisissant cette variable !
 gm_script_path="/home/dorian/.mozilla/firefox/8ned6ue0.default/gm_scripts/JVC_SpawnKill/"
-dev_script_path="/media/data/www/spawnkill/"
+
+# répertoire de développement du script
+dev_script_path="/var/www/my-www/spawnkill/"
+
+# nom du fichier principal du script
 main_script_file="jvc-spawnkill.user.js"
