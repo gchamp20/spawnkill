99622e3664efcf5da2c992eeec25f827517dbf0a
dorian-marchal <marchal.dorian@gmail.com>
2015-02-01 14:33:48 +0100

    Adapte InfiniteSurveys au style général de SpawnKill

--- modules/InfiniteSurveys.js
+++ modules/InfiniteSurveys.js
@@ -18,12 +18,15 @@ SK.moduleConstructors.InfiniteSurveys.prototype.addMoreChoices = function() {
 	// Lors du clic sur le bouton "ajouter un sondage", on ajoute un attribut "data-id" aux 2 premières réponses
 	// afin de pouvoir éventuellement les supprimer correctement par la suite
 	$("button.btn-ajouter-sondage").click(function() {
-		if($("div#topic_sondage_form div.form-group").length == 3) {
-			$("div#topic_sondage_form div.form-group:nth-child(2)").attr("data-id", "1");
-			$("div#topic_sondage_form span.href-delete-reponse:first").attr("data-id", "1");
 
-			$("div#topic_sondage_form div.form-group:last").attr("data-id", "2");
-			$("div#topic_sondage_form span.href-delete-reponse:last").attr("data-id", "2");
+		var $sondageForm = $("div#topic_sondage_form");
+
+		if($sondageForm.find("div.form-group").length === 3) {
+			$sondageForm.find("div.form-group:nth-child(2)").attr("data-id", "1");
+			$sondageForm.find("span.href-delete-reponse:first").attr("data-id", "1");
+
+			$sondageForm.find("div.form-group:last").attr("data-id", "2");
+			$sondageForm.find("span.href-delete-reponse:last").attr("data-id", "2");
 		}
 	});
 
@@ -31,13 +34,17 @@ SK.moduleConstructors.InfiniteSurveys.prototype.addMoreChoices = function() {
 	var divId = 3;
 
 	$("span.href-ajouter-reponse").click(function() {
+
+		var $deleteResponse = $("span.href-delete-reponse");
+
 		// Lors du clic sur le bouton "ajouter une réponse", on ajoute manuellement un div.form-group contenant un input
 		$($("div#topic_sondage_form .form-group:last")[0].outerHTML).insertAfter("div#topic_sondage_form .form-group:last");
 		$("div.form-group:last").attr("data-id", divId);
-		$("span.href-delete-reponse:last").attr("data-id", divId);
+		$deleteResponse.last().attr("data-id", divId);
 
-		$("span.href-delete-reponse").off("click");
-		$("span.href-delete-reponse").click(function() {
+		$deleteResponse.off("click");
+
+		$deleteResponse.click(function() {
 			// On permet la suppression de ce div seulement s'il y a au moins 3 réponses
 			if($("div#topic_sondage_form .form-group").length > 3) {
 				$("div#topic_sondage_form .form-group[data-id='" + $(this).attr("data-id") + "']").remove();
