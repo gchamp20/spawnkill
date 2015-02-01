e45bfaca10f8d03453f29c288e1f29e6ddddee0e
dorian-marchal <marchal.dorian@gmail.com>
2015-02-01 18:24:34 +0100

    Ajoute une limite au nombre d'options d'un sondage

--- modules/InfiniteSurveys.js
+++ modules/InfiniteSurveys.js
@@ -6,14 +6,18 @@ SK.moduleConstructors.InfiniteSurveys = SK.Module.new();
 
 SK.moduleConstructors.InfiniteSurveys.prototype.id = "InfiniteSurveys";
 SK.moduleConstructors.InfiniteSurveys.prototype.title = "Sondages à plus de 5 réponses";
-SK.moduleConstructors.InfiniteSurveys.prototype.description = "Possibilité d'ajouter des sondages ayant plus de 5 réponses possibles";
+SK.moduleConstructors.InfiniteSurveys.prototype.description = "Possibilité d'ajouter des sondages ayant jusqu'à 10 réponses possibles";
 SK.moduleConstructors.InfiniteSurveys.prototype.required = false;
+
+SK.moduleConstructors.InfiniteSurveys.prototype.maxNumberOfChoices = 10;
 
 SK.moduleConstructors.InfiniteSurveys.prototype.init = function() {
 	this.addMoreChoices();
 };
 
 SK.moduleConstructors.InfiniteSurveys.prototype.addMoreChoices = function() {
+
+	var self = this;
 
 	// Lors du clic sur le bouton "ajouter un sondage", on ajoute un attribut "data-id" aux 2 premières réponses
 	// afin de pouvoir éventuellement les supprimer correctement par la suite
@@ -35,23 +39,30 @@ SK.moduleConstructors.InfiniteSurveys.prototype.addMoreChoices = function() {
 
 	$("span.href-ajouter-reponse").click(function() {
 
-		var $deleteResponse = $("span.href-delete-reponse");
+		var $sondageForm = $("div#topic_sondage_form");
+		var numberOfChoices = $sondageForm.find(".form-group").length - 1;
 
-		// Lors du clic sur le bouton "ajouter une réponse", on ajoute manuellement un div.form-group contenant un input
-		$($("div#topic_sondage_form .form-group:last")[0].outerHTML).insertAfter("div#topic_sondage_form .form-group:last");
-		$("div.form-group:last").attr("data-id", divId);
-		$deleteResponse.last().attr("data-id", divId);
+		// On limite le nombre de choix
+		if (numberOfChoices < self.maxNumberOfChoices) {
 
-		$deleteResponse.off("click");
+			// Lors du clic sur le bouton "ajouter une réponse", on ajoute manuellement un div.form-group contenant un input
+			$($sondageForm.find(".form-group:last")[0].outerHTML).insertAfter($sondageForm.find(".form-group:last"));
+			$("div.form-group:last").attr("data-id", divId);
 
-		$deleteResponse.click(function() {
-			// On permet la suppression de ce div seulement s'il y a au moins 3 réponses
-			if($("div#topic_sondage_form .form-group").length > 3) {
-				$("div#topic_sondage_form .form-group[data-id='" + $(this).attr("data-id") + "']").remove();
-			}
-		});
+			var $deleteResponse = $("span.href-delete-reponse");
 
-		divId++;
+			$deleteResponse.last().attr("data-id", divId);
+			$deleteResponse.off("click");
+			$deleteResponse.click(function() {
+				// On permet la suppression de ce div seulement s'il y a au moins 3 réponses
+				if($sondageForm.find(".form-group").length > 3) {
+					$sondageForm.find(".form-group[data-id='" + $(this).attr("data-id") + "']").remove();
+				}
+			});
+
+			divId++;
+		}
+
 		return false; // Permet d'éviter l'action normale du bouton "ajouter une réponse"
 	});
 };
